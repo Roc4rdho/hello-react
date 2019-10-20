067ec896a1dd8192eb7899bd781e9e756d201c90
Fonji Fonkeng Rocard <rocardpp@gmail.com>
2019-10-20 22:02:11 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+	<p>Hello React, From Google Kubernetes Engine</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Roc4rdho/hello-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git Repo
         </a>
       </header>
     </div>
